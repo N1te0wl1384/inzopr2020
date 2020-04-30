031a07dfbe8786d795a49cdf9d3714ff6583e1ca
Marek <marek_koga@interia.pl>
2020-04-30 16:03:08 +0200

    adding get_info function

--- git_operations/conflict_file.py
+++ git_operations/conflict_file.py
@@ -1,7 +1,13 @@
+def get_info():
+    print("Witaj, to jest prosty kalkulator")
+
+
 def dodaj(a, b):
     print("wprowadz liczby")
     a=int(input())
     b=int(input())
     print(a+b)
 
+
+get_info()
 dodaj(3, 5)
